d982295282156f0d1708700ed5e7e682c15c32e4
Dozen233 <1965037218@qq.com>
2022-06-24 09:40:22 +0800

    添加idfs工程文件结构

---
+++ idfs/src/main/java/com/idforg/idfs/IdfsApplication.java
@@ -0,0 +1,13 @@
+package com.idforg.idfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdfsApplication.class, args);
+    }
+
+}
---
+++ idfs/src/test/java/com/idforg/idfs/idfs/IdfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idforg.idfs.idfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdfsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
